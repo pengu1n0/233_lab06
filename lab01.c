072a990575f8ee045649c6a8dbcc52a7c9368e41
pengu1n0 <78530429+pengu1n0@users.noreply.github.com>
2021-03-18 14:55:42 +0700

    Create lab01.c

---
+++ lab01.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int i,j,count;
+    char a[100][100],b[1000];
+    scanf("%d",&count);
+    for(i=0;i<=count-1;i++)
+    {
+        scanf("%s" ,a[i]);
+    }
